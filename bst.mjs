d5fbc4f8c6cd0bca356eb5d04686055495cada25
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-03-25 13:50:30 +0700

    insert deleteItem find

--- bst.mjs
+++ bst.mjs
@@ -33,18 +33,72 @@ class Tree {
     return this.root;
   }
 
-  logTreeOrder() {
-    const result = [];
-    const catArray = (node = this.root) => {
-      if (node === null) return;
+  insert(value, node = this.root) {
+    if (node === null) {
+      return (node = new Node(value));
+    }
 
-      catArray(node.left);
-      result.push(node.data);
-      catArray(node.right);
-    };
-    catArray();
+    if (node.data > value) node.left = this.insert(value, node.left);
+    if (node.data < value) node.right = this.insert(value, node.right);
 
-    console.log('logTreeOrder', result);
+    return node;
+  }
+
+  deleteItem(value, node = this.root) {
+    if (node === null) {
+      return null;
+    }
+
+    if (node.data > value) node.left = this.deleteItem(value, node.left);
+    if (node.data < value) node.right = this.deleteItem(value, node.right);
+
+    if (node.data === value && node.left && node.right) {
+      let replacement = node.right;
+      let previous = node;
+      while (replacement.left !== null) {
+        previous = replacement;
+        replacement = replacement.left;
+      }
+      if (replacement.right) {
+        previous.left = replacement.right;
+      } else {
+        previous.left = null;
+      }
+      node.data = replacement.data;
+      return node;
+    }
+
+    if (node.data === value && (!node.left || !node.right)) {
+      return (node = node.left || node.right);
+    }
+
+    if (node.data === value) return (node = null);
+
+    return node;
+  }
+
+  find(value, node = this.root) {
+    if (node === null) return null;
+
+    if (node.data < value) {
+      this.find(value, node.right);
+    }
+
+    if (node.data < value) {
+      this.find(value, node.left);
+    }
+
+    if (node.data === value) return console.log('find', node);
+  }
+
+  logTreeOrder(node = this.root, result = []) {
+    if (node === null) return;
+
+    this.logTreeOrder(node.left, result);
+    result.push(node.data);
+    this.logTreeOrder(node.right, result);
+
+    if (node === this.root) console.log('logTreeOrder', result);
   }
 
   prettyPrint(node = this.root, prefix = '', isLeft = true) {
@@ -70,4 +124,15 @@ const array1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree1 = new Tree(array1);
 
 tree1.prettyPrint();
-tree1.logTreeOrder();
+// tree1.logTreeOrder();
+// tree1.insert(6);
+// tree1.insert(2);
+tree1.deleteItem(67);
+tree1.deleteItem(1);
+tree1.deleteItem(23);
+tree1.deleteItem(8);
+tree1.deleteItem(7);
+tree1.prettyPrint();
+// tree1.logTreeOrder();
+tree1.find(324);
+console.log(tree1);
